f1571885a69c3132dec2d6da65fdf9a511421b67
DukiChoi <cbcc1234@naver.com>
2021-09-15 22:59:37 +0900

    21-09-15 10:59 keyboard input

--- gesture_code/gesture.py
+++ gesture_code/gesture.py
@@ -2,15 +2,18 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pyautogui as mouse
+import time
 
-
+#숫자 입력한 시간 체크
+count = 0
+keyboard_input = 0
 # 2개 손 활용
 max_num_hands = 2
 
 # gesture 기존 값 활용
 coin_gesture = {
     0:'zero', 1:'one', 9:'two', 2:'two', 3:'three', 4:'four', 5:'five',
-    6:'delete', 8:'mouse_left', 10:'ok'
+    6:'delete', 8:'mouse_left(spiderman)', 10:'ok'
 }
 
 # MediaPipe hands model
@@ -84,27 +87,101 @@ while cap.isOpened():
                 cv2.putText(img, text='(%.2f, %.2f)'%(x,y), org=(org[0], org[1] - 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
 
                 num = idx
+
+                if num == 9:
+                    num = 2
+
+                if 0 <= num < 6:
+                    total_num += num
+
                 '--------------------------------------'
                 #마우스 움직여주기!!!!
                 if num == 1:
                     mouse.moveTo(2560*x,1440*y,0.1)
-                if num == 8:
+                elif num == 8:
                     mouse.click(2560*x,1440*y, button='left')
+
+                #키보드 입력 시작하기 위해 ok 사인으로 입력 여부 결정
+                if(num == 10 and keyboard_input == 0):
+                    count = count +1
+                    if count == 100:
+                        print('Get keyboardinput')
+                        keyboard_input = 1
+                        count = 0
+                elif (num == 10 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 100:
+                        print('No keyboardinput')
+                        keyboard_input = 0
+                        count = 0
+               #여기서부터는 진짜 키보드 입력
+                if num == 6:
+                    count = count + 1
+                    if count == 50:
+                        print('delete')
+                        count = 0
+                elif (total_num == 1 and keyboard_input == 1):
+                    count = count+1
+                    if count == 50:
+                        print(1)
+                        count = 0
+                elif (total_num == 2 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(2)
+                        count = 0
+                elif (total_num == 3 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(3)
+                        count = 0
+                elif (total_num == 4 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(4)
+                        count = 0
+                elif (total_num == 5 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 100:
+                        print(5)
+                        count = 0
+                elif (total_num == 6 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(6)
+                        count = 0
+                elif (total_num == 7 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(7)
+                        count = 0
+                elif (total_num == 8 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(8)
+                        count = 0
+                elif (total_num == 9 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 50:
+                        print(9)
+                        count = 0
+                elif (total_num == 0 and keyboard_input == 1):
+                    count = count + 1
+                    if count == 100:
+                        print(0)
+                        count = 0
+
                 '--------------------------------------'
 
 
 
-                if num == 9:
-                    num = 2
-                
-                if 0 <= num < 6:
-                    total_num += num
+
 
 
             mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
-            
 
-        if total_num != -1:
+
+        if total_num != 0:
             cv2.putText(img, text="Total Num is %i"%(total_num), org=(int(img.shape[1] / 3), int(img.shape[0] / 3)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=(0, 0, 255), thickness=3)
 
 
